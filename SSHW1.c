ae037853a1c1fdcadc6d69da9524404555b489d1
Liam May <liam_may@knights.ucf.edu>
2020-01-31 02:27:41 -0500

    Update SSHW1.c
    I solved 90% of our file read problems (the last part of the problem is on line 190) and I figure the next item on the to do list is figuring out the final structure of fetch and execute because we need to call fetch from inside of the while loop on line 86 if we want it to work. We're making progress!

--- SSHW1.c
+++ SSHW1.c
@@ -3,291 +3,194 @@
 // COP 3402
 // Spring 2020
 
-#define MAX_DATA_STACK_HEIGHT 23
-#define MAX_CODE_LENGTH 500
-#define MAX_LEXI_LEVELS 3
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
-typedef struct oplm {
-    int op; // opcode
-    int reg; // register
-    int l; // L
-    int m; // M
-}instruction;
+#define MAX_DATA_STACK_HEIGHT 23
+#define MAX_CODE_LENGTH 500
+#define MAX_LEXI_LEVELS 3
 
-instruction *create_instruction(int op, int reg, int l, int m);
-void fetchCycle(char *str);
-void executionCycle(instruction *ir);
-instruction *toInstruction(char *str);
-void printInstructions(instruction *arr, int len);
-void burnIt(instruction *arr, int len);
+void fetchCycle(int *arr, int size);
+void executionCycle(int op, int reg, int l, int m);
 
-instruction *create_instruction(int op, int reg, int l, int m)
+// Takes in an array of integers representing instructions and sends them to
+// executionCycle to be processed
+void fetchCycle(int *arr, int len)
 {
-  instruction *i = calloc(1, sizeof(instruction));
-  i->op = op;
-  i->reg = reg;
-  i->l = l;
-  i->m = m;
+  // capturing the length of the int array
+  int i, op = 0, r = 0, l = 0, m = 0;
 
-  return i;
-}
+  printf("\n\nSize of array: %d\nValues successfully passed:\n======================\n", len);
 
-// iterates through array of instructions and sends them to executionCycle
-void fetchCycle(char *str)
-{
-  int i, len = strlen(str);
-  char *token;
-  int insts[len];
-
+  // loops through all integers in arr and sends the values in sets of four to
+  // executionCycle
   for (i = 0; i < len; i++)
   {
-    token = strtok(str, " ");
-    printf("token: %s\n", token);
-    insts[i] = atoi(token);
+    if (i % 4 == 0)
+    {
+      op = arr[i];
+      // continue;
+    }
+    if (i % 4 == 1)
+    {
+      r = arr[i];
+      // continue;
+    }
+    if (i % 4 == 2)
+    {
+      l = arr[i];
+      // continue;
+    }
+    if (i % 4 == 3)
+    {
+      m = arr[i];
+      // continue;
+    }
+    printf("calling executionCycle...\n"); // why are these lines not executing?
+    executionCycle(op, r, l, m);
   }
-  printf("\nInteger array of instructons:\n");
-  for (i = 0; i < len; i++)
-  {
-    (i % 4 == 0) ? printf("%d\n", insts[i]) : printf("%d ", insts[i]);
-  }
-
-
-  // int i;
-  //
-  // for (i = 0; i < len; i++)
-  // {
-  //   executionCycle(&theseInstructions[i]);
-  // }
-  //
-  // return;
+  return;
 }
 
 int base(int l, int base, int* data_stack)
 // l stand for L in the instruction format
 {
-    int b1; //find base L levels down
-    b1 = base;
-    while (l > 0)
-    {
-        b1 = data_stack[b1 + 1];
-        l--;
-
-    }
-    return b1;
+  int b1; //find base L levels down
+  b1 = base;
+  while (l > 0)
+  {
+    b1 = data_stack[b1 + 1];
+    l--;
+  }
+  return b1;
 }
 
 // takes in a single instruction and executes the command of that instruction
-void executionCycle(instruction *inst)
+void executionCycle(int op, int r, int l, int m)
 {
-  int sp, bp, pc, ir, gp, halt, m;
-  int data_stack[41], reg[200];
-
-  for(int a = 0; a<40; a++)
-  {
-     data_stack[a] = 0;
-  }
+  int sp, bp, pc, ir, gp, halt;
+  int data_stack[41] = {0}, reg[200];
 
   sp = MAX_DATA_STACK_HEIGHT;
   bp = 0;
   pc = 0;
   ir  = 0;
   gp = -1;
-  halt = 1;
+  halt = 0;
   int i = 0;
 
-  while(halt == 1)
+  // printf("< %d , %d , %d , %d >\n", op, r, l, m);
+
+  while (halt == 0) // This is an infinite loop right now because we need to be
+                    // calling fetchCycle from within this function, not the other
+                    // way around. But I am tired so I leave that for tomorrow
   {
 
-   switch(inst->op){
+    switch(op)
+    {
+       case 1:
+        reg[i] = m;
+        break;
 
-   case 1:
+       case 2:
+        sp = bp - 1;
+        bp = data_stack[sp + 3];
+        pc = data_stack[sp + 4];
+        break;
 
-    reg[i] = m;
-    break;
+       case 3:
+        reg[i] = data_stack[base(l, bp, data_stack) + m];
+        break;
 
-   case 2:
+       case 4:
+        data_stack[ base(l, bp, data_stack) + m] = reg[i];
+        break;
 
-    sp = bp - 1;
-    bp = data_stack[sp + 3];
-    pc = data_stack[sp + 4];
-    break;
+       case 5:
+        data_stack[sp + 1]  = 0;
+        data_stack[sp + 2]  =  base(l, bp, data_stack);
+        //base is a function
+        data_stack[sp + 3]  = bp;
+        data_stack[sp + 4]  = pc;
+        bp = sp + 1;
+        pc = m;
+        break;
 
-   case 3:
+       case 6:
+         sp = sp + m;
+         break;
 
-    reg[i] = data_stack[base(inst->l, bp, data_stack) + m];
-    break;
+       case 7:
+         pc = m;
+         break;
 
-   case 4:
+       case 8:
+         if(reg[i] == 0)
+         {
+             pc = m;
+         }
+         break;
 
-    data_stack[ base(inst->l, bp, data_stack) + m] = reg[i];
-    break;
+       case 9:
+         printf("%d", reg[i]);
+         break;
 
-   case 5:
+       case 10:
+         //new version says read reg[i];
+         //In class, he says to scan and output something, not sure what to output
+         scanf("%d", &reg[i]);
+         break;
 
-    data_stack[sp + 1]  = 0;
-    data_stack[sp + 2]  =  base(inst->l, bp, data_stack);
-    //base is a function
-    data_stack[sp + 3]  = bp;
-    data_stack[sp + 4]  = pc;
-    bp = sp + 1;
-    pc = m;
-    break;
-
-   case 6:
-
-       sp = sp + m;
-       break;
-
-   case 7:
-
-       pc = m;
-       break;
-
-   case 8:
-
-       if(reg[i] == 0)
-       {
-           pc = m;
-       }
-       break;
-
-   case 9:
-
-       printf("%d", reg[i]);
-       break;
-
-   case 10:
-
-       //new version says read reg[i];
-       //In class, he says to scan and output something, not sure what to output
-       scanf("%d", &reg[i]);
-       break;
-
-    default:
-
-        halt = 0;
-
-       }
-    }
+        default:
+          halt = 1;
+      }
+  }
+  return;
 }
-
-// Takes in string of integers representing instructions.
-// Then parses the integer values out of the string
-// and assigns them to the corresponding values within an instruction struct.
-// This function returns an ordered array of all of the instructions.
-// instruction *toInstruction(char *str)
-// {
-//   int count = 1, op, reg, l, m, len = strlen(str), i = 1;
-//   instruction *thisInst;
-//
-//   printf("1\n");
-//   instruction *theseInstructions = malloc((sizeof(instruction) * len) + 1);
-//   printf("2\n");
-//   char *token;
-//   printf("4\n");
-//   while (token != NULL)
-//   {
-//     printf("5\n");
-//     token = strtok(str, " ");
-//     if (count % 4 == 1)
-//     {
-//       // putting every first value in op field
-//       op = atoi(token);
-//     }
-//     printf("6\n");
-//     if (count % 4 == 2)
-//     {
-//       // putting every second value in the reg field
-//       reg = atoi(token);
-//     }
-//
-//     if (count % 4 == 3)
-//     {
-//       // putting every third value in the l field
-//       l = atoi(token);
-//     }
-//     if (count % 4 == 0)
-//     {
-//       // putting every fourth value in the m field
-//       m = atoi(token);
-//     }
-//     // creating an instruction object with the four fields
-//     *thisInst = *create_instruction(op, reg, l, m);
-//     // adding this instruction object to the array of instructions before the
-//     // fields get overwritten with the next set of instructions
-//     theseInstructions[i++] = *thisInst;
-//     printf("loop %d...\n", i++);
-//     count++;
-//   }
-//   return theseInstructions;
-// }
-
-// void printInstructions(instruction *arr, int len)
-// {
-//   int i;
-//   for (i = 0; i < len; i++)
-//   {
-//     printf("instruction %d: %d %d %d %d\n", i, arr[i].op, arr[i].reg, arr[i].l, arr[i].m);
-//   }
-// }
-
-// frees all of the dynamically allocated memory from the instruction array
-// void burnIt(instruction *arr, int len)
-// {
-//   int i;
-//   for (i = 0; i < len; i++)
-//   {
-//     free(&arr[i]);
-//   }
-//   free(arr);
-// }
 
 int main(int argc, char **argv)
 {
   // Reading the instrustions
-  int i = 0, j = 0, len, retval;
+  int i = 0, j = 1, len, num, insts[MAX_CODE_LENGTH];
   char *filename = argv[1], buffer[MAX_CODE_LENGTH] = {'\0'}, c;
   FILE *fp;
-  instruction *theseInstructions;
 
+  // opening the file
   if (fp != NULL)
   {
     fp = fopen(filename, "r");
-    // printf("File opened\n");
   }
   else
   {
-    printf("err: file not found");
+    printf("err: file not found\n");
     return 0;
   }
 
-  // Collecting file input
+  printf("Contents being scanned into array:\n");
+  // Copying the file contents into an integer array
+  // fscanf called here to get initial value that is evaluated in order to start
+  // the loop
+  fscanf(fp, "%d", &num);
   while (!feof(fp))
   {
-    // reading in the file in character by character
-    c = fgetc(fp);
-    buffer[i] = c;
-    i += 1;
-  }
-  len = strlen(buffer);
+    // Prints values as they are scanned in rows of 4
+    (j++ % 4 == 0) ? printf("%d\n", num) : printf("%d ", num);
 
-  // converting char input to ints then to instructions and storing those
-  // instructions in an array
-  // printf("toInstruction...\n");
-  // theseInstructions = toInstruction(buffer);
-  // testing toInstruction by printing array of instructions
-  // printf("printInstructions...\n");
-  // printInstructions(theseInstructions, len);
-  // sending array of instructions to be processed
-  printf("File contents:\n*%s*\n", buffer);
-  fetchCycle(buffer);
-  // printf("burnIt...\n");
-  // burnIt(theseInstructions, len);
+    insts[i++] = num;
+    fscanf(fp, "%d", &num);
+  }
+  // Prints last value scanned
+  printf("%d\n", num);
+
+  printf("\n\nContents of insts array after transfer:\n");
+  for (i = 0; i < j; i++)
+  {
+    printf("%d\n", insts[i]); // I guess the last value in the array is garbage?
+  }
+
+  fetchCycle(insts, j);
 
   fclose(fp);
   return 0;
